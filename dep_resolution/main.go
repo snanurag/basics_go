35834307c8b8a2000ad75b908306214d30c8d494
Anurag Shrinagar <ashrinagar@ashrinagar-a01.vmware.com>
2019-11-21 09:21:20 +0530

    committing dep resolution test

---
+++ dep_resolution/main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Uncomment and compile. See the difference in size.
+	// Deleting or adding functions before uncommenting it doesn't give difference in size.
+
+	// printme()
+
+}
+
+func printme() {
+	fmt.Print("Hello world")
+	printme1()
+}
+
+func printme1() {
+	fmt.Print("Hello world")
+	printme2()
+}
+func printme2() {
+	fmt.Print("Hello world")
+	printme3()
+}
+func printme3() {
+	fmt.Print("Hello world")
+	printme4()
+}
+func printme4() {
+	fmt.Print("Hello world")
+	printme5()
+}
+func printme5() {
+	fmt.Print("Hello world")
+	printme6()
+}
+func printme6() {
+	fmt.Print("Hello world")
+	printme7()
+}
+func printme7() {
+	fmt.Print("Hello world")
+	printme8()
+}
+func printme8() {
+	fmt.Print("Hello world")
+	printme9()
+}
+func printme9() {
+	fmt.Print("Hello world")
+	printme10()
+}
+func printme10() {
+	fmt.Print("Hello world")
+}
