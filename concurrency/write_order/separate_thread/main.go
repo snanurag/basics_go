ced08fa3e6360bae184f180374e6e9660a4a9468
ashrinagar <ashrinagar@vmware.com>
2020-01-31 14:38:36 +0530

    Removed unnecessary comments

--- concurrency/write_order/separate_thread/main.go
+++ concurrency/write_order/separate_thread/main.go
@@ -1,34 +1,25 @@
 package main
-import (
-//	"runtime"
-	// "sync"
-)
+
 var a, b int
-// var wg sync.WaitGroup
 
 func f() {
 	a = 1
 	b = 2
-	// wg.Done()
 }
 
-
 func main() {
-//	runtime.GOMAXPROCS(1)
+	//runtime.GOMAXPROCS(1)
 	for {
-		// wg.Add(1)
 		go f()
-		
-		if b ==2 && a == 0 {
-			println(b ," " , a)
+
+		if b == 2 && a == 0 {
+			println(b, " ", a)
 			println("found condition : write order is not reflected during read.")
 			break
 		}
-		println(b ," " , a)
-		// wg.Wait()
+		println(b, " ", a)
 		a = 0
 		b = 0
-		
 
 	}
 }
