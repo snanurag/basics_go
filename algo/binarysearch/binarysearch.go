582f879da635ad924217ae51a2b5aab54c5e8912
Anurag Shrinagar <ashrinagar@vmware.com>
2020-12-14 20:22:56 +0530

    A generic binary search

--- algo/binarysearch/binarysearch.go
+++ algo/binarysearch/binarysearch.go
@@ -1,6 +1,7 @@
 package binarysearch
 
-func binarySearch(needle int, haystack []int) bool {
+// Returns if the value is present or not and the index where value is present. If needle is not present then it returns false, -ve of index where it should be present.
+func BinarySearch(needle int, haystack []int) (bool, int) {
 
 	low := 0
 	high := len(haystack) - 1
@@ -16,8 +17,8 @@ func binarySearch(needle int, haystack []int) bool {
 	}
 
 	if low == len(haystack) || haystack[low] != needle {
-		return false
+		return false, -low
 	}
 
-	return true
+	return true, low
 }
